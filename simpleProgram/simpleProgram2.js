ca1bc5077c5f4db1c2d4dc299ba2bb700a150de6
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-06-12 11:21:44 +0530

    simpleProgram.ts

--- simpleProgram/simpleProgram2.js
+++ simpleProgram/simpleProgram2.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const x = 101;
-x = "Pranab"; /* We can do the type check by doing tsc -b. */
-console.log(x);
+x = "Pranab"; /* We can do the type check by doing tsc -b, that's why Typescript has been introduced. */
+console.log(x); /* We can't pass String in a variable which was declared to hold the Number type, before it was possible while we were using only Javascript. */
 //# sourceMappingURL=simpleProgram2.js.map
