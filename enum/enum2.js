5e6f99a53860928188fb16bb7fdaa748db61d5e9
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-06-15 11:29:03 +0530

    enum.ts

--- enum/enum2.js
+++ enum/enum2.js
@@ -1,17 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Direction;
-(function (Direction) {
+var Direction1;
+(function (Direction1) {
     /* Enum help to use the more human-readable constants which can fit the scenario. */
-    Direction[Direction["Up"] = 0] = "Up";
-    Direction[Direction["Down"] = 1] = "Down";
-    Direction[Direction["Left"] = 2] = "Left";
-    Direction[Direction["Right"] = 3] = "Right";
-})(Direction || (Direction = {}));
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
 function doSomething(keyPressed) {
-    if (keyPressed == Direction.Up) {
+    if (keyPressed == Direction1.Up) {
     }
 }
-doSomething(Direction.Up);
-doSomething(Direction.Right);
+doSomething(Direction1.Up);
+doSomething(Direction1.Right);
+console.log(Direction1.Up);
+console.log(Direction1.Down);
+console.log(Direction1.Left);
+console.log(Direction1.Right);
+console.log();
+var Direction2;
+(function (Direction2) {
+    Direction2[Direction2["Up"] = 3] = "Up";
+    Direction2[Direction2["Down"] = 4] = "Down";
+    Direction2[Direction2["Left"] = -9] = "Left";
+    Direction2[Direction2["Right"] = -8] = "Right"; /* Here it will be -8. */
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Up);
+console.log(Direction2.Down);
+console.log(Direction2.Left);
+console.log(Direction2.Right);
+console.log();
+var Direction3;
+(function (Direction3) {
+    Direction3["Up"] = "up";
+    Direction3[Direction3["Down"] = void 0] = "Down";
+    Direction3[Direction3["Left"] = void 0] = "Left";
+    Direction3[Direction3["Right"] = void 0] = "Right";
+})(Direction3 || (Direction3 = {}));
 //# sourceMappingURL=enum2.js.map
--- enum/enum2.ts
+++ enum/enum2.ts
@@ -1,20 +1,50 @@
-enum Direction {                                /* This is the more cleaner solution of this game problem, where we are using enum. */
-                                                /* Enum help to use the more human-readable constants which can fit the scenario. */
-  Up,
-  Down,
+enum Direction1 {                                /* This is the more cleaner solution of this game problem, where we are using enum. */
+                                                 /* Enum help to use the more human-readable constants which can fit the scenario. */
+  Up,                                            /* Bydefault enum starts to give value from the first constant as 0 and then increment as the constants present at the below like 1, 2, 3 .... and so on. */
+  Down,                                          /* But we can also give values as per our wish in any order, not neccessarily need it to be in increment order. */
   Left,
   Right
 
 }
 
 
-function doSomething(keyPressed: Direction) {
+function doSomething(keyPressed: Direction1) {   /* Here we can put the logic of the pressed button. */
 
-  if(keyPressed == Direction.Up) {
+  if(keyPressed == Direction1.Up) {             
 
 
   }
 
 }
-doSomething(Direction.Up);
-doSomething(Direction.Right);
+doSomething(Direction1.Up);
+doSomething(Direction1.Right);
+console.log(Direction1.Up);
+console.log(Direction1.Down);
+console.log(Direction1.Left);
+console.log(Direction1.Right);
+
+console.log();
+
+enum Direction2 {
+
+  Up = 3,
+  Down,                                          /* Here the value will be after incrementing the value which been hold by it's previous constant. */
+  Left = -9,                                     /* Here it holding "-9". */
+  Right                                          /* Here it will be -8. */
+
+}
+console.log(Direction2.Up);
+console.log(Direction2.Down);
+console.log(Direction2.Left);
+console.log(Direction2.Right);
+
+console.log();
+
+enum Direction3 {
+
+  Up = "up",                                     /* In previous scenario we have seen that, we can put values(number) type to constants at start and then we can either give values to other constants or not based on our own wish. */
+  Down,                                          /* But let's suppose we have given the value(string) type to constant then the Typescript can't guess automatically which would be the next value for nect constant, like the way it was doing in the case of number data type. */
+  Left,
+  Right,
+
+}
